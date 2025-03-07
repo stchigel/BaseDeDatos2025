b9f6863a0fe840fdf6c0732ace6a7993eff7d75b
Nico <166736520+stchigel@users.noreply.github.com>
2025-03-07 11:53:13 -0300

    Clase 7/3/25 (agregado vistas)

--- Queries repaso.sql
+++ Queries repaso.sql
@@ -1,4 +1,4 @@
-/*1*/ select nombre from proovedor where ciudad = "La Plata";
+/*1*/ select nombre from proveedor where ciudad = "La Plata";
 /*2*/ delete from articulo where (select material_codigo from compuesto_por where articulo_codigo = codigo) is null;
 /*3*/ select codigo, descripcion from articulo join compuesto_por on codigo=articulo_codigo join provisto_por on provisto_por.material_codigo = compuesto_por.material_codigo where proveedor_codigo = (select codigo from proveedor where nombre="Lopez SA");
 /*4*/ select proveedor.codigo, proveedor.nombre from proveedor 
@@ -19,3 +19,7 @@ order by (select count(compuesto_por.material_codigo) from compuesto_por where a
 /*13*/ select sum(precio*stock) from articulo join tiene on articulo.codigo=articulo_codigo group by articulo_codigo having sum(stock) > 100;
 /*14*/ select * from articulo where precio>5000 having (select count(compuesto_por.material_codigo) as cant from compuesto_por where articulo_codigo=articulo.codigo)>3;
 /*15*/ select material_codigo from compuesto_por join articulo on articulo_codigo=articulo.codigo where articulo.precio > (select avg(precio) from tiene join articulo on articulo_codigo=articulo.codigo where almacen_codigo=2);
+
+/*prueba view*/
+create view ej1 as select nombre from proveedor where ciudad = "La Plata";
+select * from ej1;
